fa4b3326af905fbd17f22dd2ef19917a4dd5bf8d
Joany Camily da Silva <joanycamily23@gmail.com>
2023-07-28 12:27:33 -0300

    continuação tcc

---
+++ AcademiaCintia/AcademiaCintia/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AcademiaCintia.Models;
+
+namespace AcademiaCintia.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult SobreNos()
+    {
+        return View();
+    }
+
+    public IActionResult Horarios()
+    {
+        return View();
+    }
+
+    public IActionResult Modalidades()
+    {
+        return View();
+    }
+
+    public IActionResult NossoTime()
+    {
+        return View();
+    }
+
+    public IActionResult TesteIMC()
+    {
+        return View();
+    }
+
+    public IActionResult Galeria()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ AcademiaCintia/AcademiaCintia/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AcademiaCintia.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
