d369d9ad44346444d6b154140e9cf26ad6898633
Joany Camily da Silva <joanycamily23@gmail.com>
2023-11-14 12:07:33 -0300

    nada

---
+++ AcademiaCintia/AcademiaCintia/Migrations/20231114131826_criando-banco.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademiaCintia.Migrations
+{
+    public partial class criandobanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
